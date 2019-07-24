f03921128024c540c9172fc7ed90efdc0b3a791b
valis <valery.isaev@gmail.com>
2019-07-24 16:35:00 +0300

    Create the site

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,6 @@
+var store = [{
+        "title": "Arend 1.0.0 released",
+        "excerpt":"  Under construction  ","categories": [],
+        "tags": [],
+        "url": "https://arend-lang.github.io/2019/07/17/Arend-1.0.0-released.html",
+        "teaser":null}]
