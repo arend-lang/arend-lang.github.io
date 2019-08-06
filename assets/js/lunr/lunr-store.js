1b1c88df1a3e8875271832eb0a86bdd7dd113f71
Valery Isaev <valery.isaev@jetbrains.com>
2019-08-06 23:24:57 +0300

    The first version of Arend is released!

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,6 +1,6 @@
 var store = [{
         "title": "Arend 1.0.0 released",
-        "excerpt":"  Under construction  ","categories": [],
+        "excerpt":"The first version of Arend is released! It implements the following features: Path types based on the interval type. Higher inductive types, including higher inductive-inductive types. Class system. Universe polymorphism. The IntelliJ Arend plugin implements the following features: Syntax highlighting Code completion Find usages Quick documentation Parameter hints Class hierarchy...","categories": [],
         "tags": [],
         "url": "https://arend-lang.github.io/2019/07/17/Arend-1.0.0-released.html",
         "teaser":null}]
