58d2dd6352b18801cb1bfcb66ad0e7e9da4fcc6d
Valery Isaev <valery.isaev@jetbrains.com>
2019-10-01 19:30:41 +0300

    Version 1.1.0

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -3,4 +3,9 @@ var store = [{
         "excerpt":"The first version of Arend is released! It implements the following features: Path types based on the interval type. Higher inductive types, including higher inductive-inductive types. Class system. Universe polymorphism. The IntelliJ Arend plugin implements the following features: Syntax highlighting Code completion Find usages Quick documentation Parameter hints Class hierarchy...","categories": [],
         "tags": [],
         "url": "https://arend-lang.github.io/2019/07/17/Arend-1.0.0-released.html",
+        "teaser":null},{
+        "title": "Arend 1.1.0 released",
+        "excerpt":"Arend now has proof irrelevant universe of proposition and the plugin can run the typechecker automatically in background. Language updates: \\Prop is now proof irrelevant. In particular, this means that we do not need Path.inProp anymore. Right operator sections. New function and \\case forms: \\sfunc and \\scase. It is necessary...","categories": [],
+        "tags": [],
+        "url": "https://arend-lang.github.io/2019/10/01/Arend-1.1.0-released.html",
         "teaser":null}]
