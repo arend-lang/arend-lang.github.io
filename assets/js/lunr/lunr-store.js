bdd6819f67ffa4ae3c1b7843e154702631e541ee
Valery Isaev <valery.isaev@jetbrains.com>
2019-12-17 01:14:49 +0300

    Version 1.2.0

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -8,4 +8,9 @@ var store = [{
         "excerpt":"Arend now has proof irrelevant universe of proposition and the plugin can run the typechecker automatically in background. Language updates: \\Prop is now proof irrelevant. In particular, this means that we do not need Path.inProp anymore. Right operator sections. New function and \\case forms: \\sfunc and \\scase. It is necessary...","categories": [],
         "tags": [],
         "url": "https://arend-lang.github.io/2019/10/01/Arend-1.1.0-released.html",
+        "teaser":null},{
+        "title": "Arend 1.2.0 released",
+        "excerpt":"We implemented a few features related to classes and pattern matching. One of these features is pattern matching on idp : a = a, which can be used instead of the J operator. Language updates: Implemented pattern matching on idp. This feature can be used instead of the J operator...","categories": [],
+        "tags": [],
+        "url": "https://arend-lang.github.io/2019/12/16/Arend-1.2.0-released.html",
         "teaser":null}]
