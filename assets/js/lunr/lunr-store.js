a2ac115757f007e95fb015600df44f7e2a76b04e
valis <valery.isaev@gmail.com>
2021-03-30 14:15:18 +0300

    Update dependencies

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -2,54 +2,54 @@ var store = [{
         "title": "Arend 1.0.0 released",
         "excerpt":"The first version of Arend is released! It implements the following features: Path types based on the interval type. Higher inductive types, including higher inductive-inductive types. Class system. Universe polymorphism. The IntelliJ Arend plugin implements the following features: Syntax highlighting Code completion Find usages Quick documentation Parameter hints Class hierarchy...","categories": [],
         "tags": [],
-        "url": "https://arend-lang.github.io/2019/07/17/Arend-1.0.0-released.html",
+        "url": "/2019/07/17/Arend-1.0.0-released.html",
         "teaser": null
       },{
         "title": "Arend 1.1.0 released",
         "excerpt":"Arend now has proof irrelevant universe of proposition and the plugin can run the typechecker automatically in background. Language updates: \\Prop is now proof irrelevant. In particular, this means that we do not need Path.inProp anymore. Right operator sections. New function and \\case forms: \\sfunc and \\scase. It is necessary...","categories": [],
         "tags": [],
-        "url": "https://arend-lang.github.io/2019/10/01/Arend-1.1.0-released.html",
+        "url": "/2019/10/01/Arend-1.1.0-released.html",
         "teaser": null
       },{
         "title": "Arend 1.2.0 released",
         "excerpt":"We implemented a few features related to classes and pattern matching. One of these features is pattern matching on idp : a = a, which can be used instead of the J operator. Language updates: Implemented pattern matching on idp. This feature can be used instead of the J operator...","categories": [],
         "tags": [],
-        "url": "https://arend-lang.github.io/2019/12/16/Arend-1.2.0-released.html",
+        "url": "/2019/12/16/Arend-1.2.0-released.html",
         "teaser": null
       },{
         "title": "The first part of our tutorial is ready",
         "excerpt":"We finished the first part of our new tutorial. It covers all the basic constructions of Arend. It does not mention anything related to homotopy theory. This will be the topic of the second part.  ","categories": [],
         "tags": [],
-        "url": "https://arend-lang.github.io/2020/01/17/tutorial-part-I.html",
+        "url": "/2020/01/17/tutorial-part-I.html",
         "teaser": null
       },{
         "title": "Arend 1.3.0 released",
         "excerpt":"We implemented language extensions. This can be used to implement custom operations on the abstract syntax tree which are not supported by the language. They can also be used to implement various decision procedures for proof automation. Language updates: Implemented language extensions. Arend now shows conditions in goals which makes...","categories": [],
         "tags": [],
-        "url": "https://arend-lang.github.io/2020/04/13/Arend-1.3.0-released.html",
+        "url": "/2020/04/13/Arend-1.3.0-released.html",
         "teaser": null
       },{
         "title": "Tutorial Part II",
         "excerpt":"The second part of our tutorial is ready.  ","categories": [],
         "tags": [],
-        "url": "https://arend-lang.github.io/2020/06/14/tutorial-part-II.html",
+        "url": "/2020/06/14/tutorial-part-II.html",
         "teaser": null
       },{
         "title": "Arend 1.4.0 released",
         "excerpt":"Language updates: Implicit lambdas. Tests directory can be used to store files with tests, examples, and other code which is not a part of the library. Improved pretty printer: if some definition is not available in the current context, it will be replaced with its full name. REPL. To run...","categories": [],
         "tags": [],
-        "url": "https://arend-lang.github.io/2020/06/29/Arend-1.4.0-released.html",
+        "url": "/2020/06/29/Arend-1.4.0-released.html",
         "teaser": null
       },{
         "title": "Arend 1.5.0 released",
         "excerpt":"Language updates: String literals, which can be used in meta code Meta resolvers, which can be used to modify the scoping rules for meta definitions \\strict parameters Improved performance Defined metas Libraries can be loaded from zip archives without unpacking Plugin updates: Completion of not-imported definitions Rainbow highlighting arend-lib can...","categories": [],
         "tags": [],
-        "url": "https://arend-lang.github.io/2020/10/10/Arend-1.5.0-released.html",
+        "url": "/2020/10/10/Arend-1.5.0-released.html",
         "teaser": null
       },{
         "title": "Arend 1.6.0 released",
         "excerpt":"Language updates: Built-in finite types \\default implementations \\coerce to function types \\coerce for fields and constructors \\have declaration Dot-syntax for dynamic definitions Added more computational rules for + and - functions Plugin updates: Quick fixes for “impossible elimination” and “expected constructor” errors API: Pattern typechecker arend-lib: Locales and topological spaces...","categories": [],
         "tags": [],
-        "url": "https://arend-lang.github.io/2021/02/28/Arend-1.6.0-released.html",
+        "url": "/2021/02/28/Arend-1.6.0-released.html",
         "teaser": null
       }]
