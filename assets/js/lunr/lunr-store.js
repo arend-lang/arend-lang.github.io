9c2d0e8e2e9d9e4f5e42f22c5f7202a325dc34d0
Valis <valery.isaev@gmail.com>
2022-12-05 00:35:39 +0100

    Add 1.9 release post

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -70,4 +70,10 @@ var store = [{
         "tags": [],
         "url": "/2022/04/15/Arend-1.8.0-released.html",
         "teaser": null
+      },{
+        "title": "Arend 1.9.0 released",
+        "excerpt":"Language updates: Properties in \\Sigma-types Definition parameters are visible in the \\where block Global level declarations Infix patterns Axioms Box expressions Property parameters Plugin updates: Inlays for levels, added parameters, and used axioms “Change arguments explicitness” refactoring “Change signature” refactoring Revealing information in error messages arend-lib: The definion of modules,...","categories": [],
+        "tags": [],
+        "url": "/2022/12/04/Arend-1.9.0-released.html",
+        "teaser": null
       }]
